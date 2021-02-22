a56d72d4bfb7dc5e0072c04f0abe54e0816d853e
Ian Macaulay <ismacaul@gmail.com>
2021-02-21 22:00:54 -0700

    Updated dotfiles

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DOTFILES_DIR=$HOME/workspaces/dotfiles
+
+# setup zshrc
+ln -s $DOTFILES_DIR/zsh/zshrc $HOME/.zshrc
+
+# setup starship
+ln -s $DOTFILES_DIR/starship.toml $HOME/.config/starship.toml
+
+# install zsh-nvm plugin
+git clone https://github.com/lukechilds/zsh-nvm.git $HOME/.zsh-nvm
+
+exec /bin/zsh
+
+ln -s $DOTFILES_DIR/Xmodmap $HOME/.Xmodmap
+ln -s $DOTFILES_DIR/tmux.conf $HOME/.tmux.conf
+
+# install node for coc-vim; zsh-nvm will install nvim
+nvm install --lts
+
+ln -s $DOTFILES_DIR/nvim $HOME/.config/nvim
+ln -s $DOTFILES_DIR/vimrc $HOME/.vimrc
