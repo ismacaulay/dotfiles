f6d54fafd06a6c70958c0c310e171a1233d39383
Ian Macaulay <ismacaul@gmail.com>
2021-03-20 13:55:24 -0600

    Updated dotfiles

---
+++ setup/setup-linux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ln -s $DOTFILES_DIR/Xmodmap $HOME/.Xmodmap
--- setup/setup.sh
+++ setup/setup.sh
@@ -2,6 +2,7 @@
 
 DOTFILES_DIR=$HOME/workspaces/dotfiles
 
+# setup git
 git config --global user.name "Ian Macaulay"
 git config --global user.email "ismacaul@gmail.com"
 git config --global pull.rebase false
@@ -9,15 +10,16 @@ git config --global pull.rebase false
 # setup zshrc
 ln -s $DOTFILES_DIR/zsh/zshrc $HOME/.zshrc
 
+# install zsh plugin
+mkdir $HOME/.zsh-plugins
+git clone https://github.com/lukechilds/zsh-nvm.git $HOME/.zsh-plugins/zsh-nvm
+git clone https://github.com/mroth/evalcache.git $HOME/.zsh-plugins/evalcache
+
 # setup starship
 ln -s $DOTFILES_DIR/starship.toml $HOME/.config/starship.toml
 
-# install zsh-nvm plugin
-git clone https://github.com/lukechilds/zsh-nvm.git $HOME/.zsh-nvm
-
 exec /bin/zsh
 
-ln -s $DOTFILES_DIR/Xmodmap $HOME/.Xmodmap
 ln -s $DOTFILES_DIR/tmux.conf $HOME/.tmux.conf
 
 # install node for coc-vim; zsh-nvm will install nvim
