b4aba7577ec884345656dbad10c112b29076f9fc
Ian Macaulay <ismacaul@gmail.com>
2024-01-09 13:22:39 -0700

    Revert "Updated dotfiles"
    This reverts commit b5e9754e26dd288b0ace82ff62695a83c5d500a0.

--- nvim/init.lua
+++ nvim/init.lua
@@ -86,16 +86,21 @@ require('lazy').setup({
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
-    -- opts = {
-    --   -- See `:help gitsigns.txt`
-    --   signs = {
-    --     add = { text = '+' },
-    --     change = { text = '~' },
-    --     delete = { text = '_' },
-    --     topdelete = { text = '‾' },
-    --     changedelete = { text = '~' },
-    --   },
-    -- },
+    opts = {
+      -- See `:help gitsigns.txt`
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      -- on_attach = function(bufnr)
+      --   vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
+      --   vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
+      --   vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+      -- end,
+    },
   },
 
   {
@@ -661,16 +666,5 @@ require('formatter').setup {
 vim.keymap.set('n', '<leader>f', function()
   vim.cmd 'Format'
 end, { desc = '[F]ormat' })
-
-require('gitsigns').setup {
-  signs = {
-    add = { text = '+' },
-    change = { text = '~' },
-    delete = { text = '_' },
-    topdelete = { text = '‾' },
-    changedelete = { text = '~' },
-  },
-}
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
